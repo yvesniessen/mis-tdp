3cd4f179b94c8b263bfff4c4e72c3d46c5d31bbf
d.simon.ac@gmail.com <d.simon.ac@gmail.com@32c75729-a2eb-40e1-671f-e14539d2368e>
2012-04-25 22:09:07 +0000

    Integration des Prototype

---
+++ trunk/MIS-TDP/MIS-TDP/ViewModel/AuftragViewModel.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Ink;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+using System.Collections.ObjectModel;
+using System.Diagnostics;
+
+namespace MIS_TDP
+{
+    public class AuftragViewModel : ViewModelBase
+    {
+        #region Constructor
+        public AuftragViewModel()
+        {
+            this.loadSampleData();
+        }
+
+        private void loadSampleData()
+        {
+            System.Uri resourceUri = new System.Uri("/MIS-TDP;component/SampleData/AuftragViewModelSampleData.xaml", System.UriKind.Relative);
+            if (System.Windows.Application.GetResourceStream(resourceUri) != null)
+            {
+                System.Windows.Application.LoadComponent(this, resourceUri);
+            }
+        }
+        #endregion 
+
+        #region properties
+        private Auftrag item;
+        public Auftrag Item
+        {
+            get
+            {
+                return item;
+            }
+            set
+            {
+                item = value;
+                OnPropertyChanged("Item");
+                Debug.WriteLine("Property: Item");
+            }
+        }
+
+        private ObservableCollection<Versicherung> VersicherungenItems = new ObservableCollection<Versicherung>();
+        public ObservableCollection<Versicherung> Versicherungen
+        {
+            get
+            {
+                return VersicherungenItems;
+            }
+            set
+            {
+                VersicherungenItems = value;
+                OnPropertyChanged("VersicherungenItems");
+                Debug.WriteLine("Property: VersicherungenItems");
+            }
+        }
+
+        private ObservableCollection<Fabrikat> FabrikateItems = new ObservableCollection<Fabrikat>();
+        public ObservableCollection<Fabrikat> Fabrikate
+        {
+            get
+            {
+                return FabrikateItems;
+            }
+            set
+            {
+                FabrikateItems = value;
+                OnPropertyChanged("FabrikateItems");
+                Debug.WriteLine("Property: FabrikateItems");
+            }
+        }
+        #endregion
+
+        #region ButtonCommands
+
+        #endregion
+    }
+}
