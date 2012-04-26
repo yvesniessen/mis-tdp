d9fcab23e2b85001ee7c9f8b5558813f30adeeea
yves.niessen@googlemail.com <yves.niessen@googlemail.com@32c75729-a2eb-40e1-671f-e14539d2368e>
2012-04-26 12:46:59 +0000

    *test for database*

---
+++ trunk/MIS-TDP/MIS-TDP/Controller/DatabaseController.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Ink;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace MIS_TDP.Controller
+{
+    public class DatabaseController
+    {
+                private const string ConnectionString = @"isostore:/Database.sdf";
+
+ 
+
+        public static void CreateDatabase()
+
+        {
+
+            using (var context = new DatabaseContext(ConnectionString))
+
+            {
+
+                if (!context.DatabaseExists())
+
+                {
+
+                    // create database if it does not exist
+
+                    context.CreateDatabase();
+
+                }
+
+            }
+
+        }
+
+ 
+
+        public static void DeleteDatabase()
+
+        {
+
+            using (var context = new DatabaseContext(ConnectionString))
+
+            {
+
+                if (context.DatabaseExists())
+
+                {
+
+                    // delete database if it exist
+
+                    context.DeleteDatabase();
+
+                }
+
+            }
+
+        }
+
+ 
+
+        public static void AddEmployee(Employee employee)
+
+        {
+
+            using (var context = new DatabaseContext(ConnectionString))
+
+            {
+
+                if (context.DatabaseExists())
+
+                {
+
+                    context.Employees.InsertOnSubmit(employee);
+
+                    context.SubmitChanges();
+
+                }
+
+            }
+
+        }
+
+ 
+
+        public static IList<Employee> GetEmployees()
+
+        {
+
+            IList<Employee> employees;
+
+            using (var context = new DatabaseContext(ConnectionString))
+
+            {
+
+                employees = (from emp in context.Employees select emp).ToList();
+
+            }
+
+ 
+
+            return employees;
+
+        }
+
+    }
+
+    }
+}
