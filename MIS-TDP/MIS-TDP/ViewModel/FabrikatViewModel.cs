600e75da65d0283aaa95edb6ca2de4c94e98ff31
yves.niessen@googlemail.com <yves.niessen@googlemail.com@32c75729-a2eb-40e1-671f-e14539d2368e>
2012-05-04 16:58:20 +0000

    *added* FabrikatPage and VM *added* VersicherungPage and VM

---
+++ MIS-TDP/MIS-TDP/ViewModel/FabrikatViewModel.cs
@@ -0,0 +1,65 @@
+﻿    using System;
+    using System.Net;
+    using System.Windows;
+    using System.Windows.Controls;
+    using System.Windows.Documents;
+    using System.Windows.Ink;
+    using System.Windows.Input;
+    using System.Windows.Media;
+    using System.Windows.Media.Animation;
+    using System.Windows.Shapes;
+    using System.Collections.ObjectModel;
+    using System.Diagnostics;
+
+    namespace MIS_TDP
+    {
+        public class FabrikatViewModel : ViewModelBase
+        {
+            #region Constructor
+            public FabrikatViewModel()
+            {
+                //this.loadSampleData();
+
+                //Todo: nur Testdaten, hier muss entsprechende ID des Auftrags geladen werden mit dem Page aufgerufen wurde
+
+            }
+
+            private void loadSampleData()
+            {
+
+            }
+            #endregion
+
+            #region properties
+
+            private TblFabrikat item;
+            public TblFabrikat Item
+            {
+                get
+                {
+                    return item;
+                }
+                set
+                {
+                    item = value;
+                    OnPropertyChanged("Item");
+                    Debug.WriteLine("Property: Item");
+                }
+            }
+
+            
+            #endregion
+
+            #region ButtonCommands
+
+            public void AddFabrikat()
+            {
+                TblFabrikat neuesFabrikat = new TblFabrikat();
+                neuesFabrikat = item;
+
+                Controller.DatabaseController.AddFabrikat(neuesFabrikat);
+            }
+
+            #endregion
+        }
+    }
