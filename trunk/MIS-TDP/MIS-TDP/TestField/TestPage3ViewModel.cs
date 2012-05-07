217a844bca60ca7e8cac5568de82a6cbd935dc6e
sch.thomas <sch.thomas@32c75729-a2eb-40e1-671f-e14539d2368e>
2012-05-07 19:28:34 +0000

    ViewModel für TestPage3

--- trunk/MIS-TDP/MIS-TDP/TestField/TestPage3ViewModel.cs
+++ trunk/MIS-TDP/MIS-TDP/TestField/TestPage3ViewModel.cs
@@ -20,65 +20,11 @@ namespace MIS_TDP
     public class TestPage3ViewModel : ViewModelBase
     {
         public void ExportToCsv()
-        {
-
-            databaseController.ReportDatabase();
-            /*
+        {            
             using (var context = new DatabaseContext(DatabaseContext.ConnectionString))
             {
-                MessageBox.Show("HALLO");
-                DataBaseReport report = new DataBaseReport();
-
-                List<TblFabrikat> temp = toList(databaseController.GetFabrikate());
-
-                foreach (TblFabrikat fab in temp)
-                {
-                    report.Fabrikate.Add(fab.Bezeichnung.ToString());
-                    Debug.WriteLine(fab.Bezeichnung);
-                }
-
-                MemoryStream ms = new MemoryStream();
-                Serialize(ms, report);
-                ms.Position = 0;
-            }
-            */
-
-            //report.XMLSerialize();
-        }
-
-        public static void Serialize(Stream streamObject, object objForSerialization)
-        {
-            using (var context = new DatabaseContext(DatabaseContext.ConnectionString))
-            {
-                if (objForSerialization == null || streamObject == null)
-                    return;
-
-                XmlSerializer serializer = new XmlSerializer(objForSerialization.GetType());
-                serializer.Serialize(streamObject, objForSerialization);
-            }
-        }
-
-        public static object Deserialize(Stream streamObject, Type serializedObjectType)
-        {
-            if (serializedObjectType == null || streamObject == null)
-                return null;
-
-            XmlSerializer serializer = new XmlSerializer(serializedObjectType);
-            return serializer.Deserialize(streamObject);
-        }
-
-
-        private static List<T> toList<T>(IEnumerable<T> enumerable)
-        {
-            var col = new List<T>();
-
-            foreach (var cur in enumerable)
-            {
-
-                col.Add(cur);
-
-            }
-            return col;
+                databaseController.ReportDatabase();
+            }            
         }
     }
 }
