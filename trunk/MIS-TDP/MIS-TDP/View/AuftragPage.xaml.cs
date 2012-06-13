d7ec622a5b222a6deaaee23df1d57095802e85fb
d.simon.ac@gmail.com <d.simon.ac@gmail.com@32c75729-a2eb-40e1-671f-e14539d2368e>
2012-06-13 15:14:47 +0000

    Kontakt auswaehlen und Telefonnummer waehlen

--- trunk/MIS-TDP/MIS-TDP/View/AuftragPage.xaml.cs
+++ trunk/MIS-TDP/MIS-TDP/View/AuftragPage.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Shapes;
 using Microsoft.Phone.Controls;
 using MIS_TDP.Controller;
 using System.Windows.Data;
+using Microsoft.Phone.Tasks;
 
 namespace MIS_TDP
 {
@@ -24,8 +25,15 @@ namespace MIS_TDP
             this.DataContext = new AuftragViewModel();
         }
 
+        Boolean blockDataReload = false;
         protected override void OnNavigatedTo(System.Windows.Navigation.NavigationEventArgs e)
         {
+            if (this.blockDataReload)
+            {
+                this.blockDataReload = false;
+                return;
+            }
+
             base.OnNavigatedTo(e);
 
             AuftragViewModel viewModel = this.DataContext as AuftragViewModel;
@@ -45,6 +53,58 @@ namespace MIS_TDP
             if (viewModel != null)
             {
                 viewModel.SaveAuftragCommand.Execute(null);
+            }
+        }
+
+        private void choosePhoneNumber_Click(object sender, RoutedEventArgs e)
+        {
+            PhoneNumberChooserTask phoneNumberChooserTask = new PhoneNumberChooserTask();
+            phoneNumberChooserTask.Completed += new EventHandler<PhoneNumberResult>(phoneNumberChooserTask_Completed);
+            phoneNumberChooserTask.Show();
+        }
+
+        String number;
+        String displayName;
+        void phoneNumberChooserTask_Completed(object sender, PhoneNumberResult e)
+        {
+            this.blockDataReload = true;
+
+            if (e.TaskResult == TaskResult.OK)
+            {
+                displayName = e.DisplayName;
+                String[] arr = e.DisplayName.Split(' ');
+
+                if (arr.Count() == 1)
+                {
+                    this.VornameTextBox.Text = arr[0];
+                }
+                if (arr.Count() == 2)
+                {
+                    this.VornameTextBox.Text = arr[0];
+                    this.NachnameTextBox.Text = arr[1];
+                }
+                else if (arr.Count() == 3)
+                {
+                    this.VornameTextBox.Text = arr[0] + " " + arr[1];
+                    this.NachnameTextBox.Text = arr[2];
+                }
+
+                number = e.PhoneNumber;
+            }
+        }
+
+        private void callPhoneNumber_Click(object sender, RoutedEventArgs e)
+        {
+            if (number != null && number.Length > 0)
+            {
+                PhoneCallTask phoneCallTask = new PhoneCallTask();
+                phoneCallTask.DisplayName = displayName;
+                phoneCallTask.PhoneNumber = number;
+                phoneCallTask.Show();
+            }
+            else
+            {
+                MessageBox.Show("Keine Nummer gespeichert !");
             }
         }
     }
